3ea9ae47cb54b7a4d299417d36517a18457c2098
Haichuan Zhang <me@std4453.com>
2019-05-21 22:38:10 +0800

    Use matter.is correctly

--- src/index.js
+++ src/index.js
@@ -31,12 +31,12 @@ const buildMap = (m) => {
                 if (begin < 0) begin = x;
             } else {
                 if (begin >= 0) {
-                    edges.push([begin, y], [x, y]);
+                    edges.push([begin, height - y], [x, height - y]);
                     begin = -1;
                 }
             }
         }
-        if (begin >= 0) edges.push([begin, y], [width, y]);
+        if (begin >= 0) edges.push([begin, height - y], [width, height - y]);
     }
     for (let x = 0; x <= width; ++x) {
         let begin = -1;
@@ -47,19 +47,20 @@ const buildMap = (m) => {
                 if (begin < 0) begin = y;
             } else {
                 if (begin >= 0) {
-                    edges.push([x, begin], [x, y]);
+                    edges.push([x, height - begin], [x, height - y]);
                     begin = -1;
                 }
             }
         }
-        if (begin >= 0) edges.push([x, begin], [x, height]);
+        if (begin >= 0) edges.push([x, height - begin], [x, height - height]);
     }
-    return { width, height, sx, sy, edges };
+    return { width, height, sx, sy: height - sy, edges };
 }
 
 (() => {
     const scene = new THREE.Scene();
     const engine = Matter.Engine.create();
+    engine.world.gravity.y = 0;
 
     const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
     const { sx, sy, edges } = buildMap(gameMap);
@@ -75,7 +76,9 @@ const buildMap = (m) => {
         cube.position.z = 10;
         scene.add(cube);
 
-        const box = Matter.Bodies.rectangle(cx - dx / 2, cy - dy / 2, dx, dy, { isStatic: true });
+        const box = Matter.Bodies.rectangle(
+            cx * 1000, -cy * 1000,
+            dx * 1000, dy * 1000, { isStatic: true });
         Matter.World.add(engine.world, box);
     }
 
@@ -91,8 +94,9 @@ const buildMap = (m) => {
     player.position.z = 0;
     scene.add(player);
 
-    const playerBody = Matter.Bodies.circle(sx, sy, 0.2);
-    Matter.Body.setMass(playerBody, 1);
+    const playerBody = Matter.Bodies.circle(
+        sx * 1000, -sy * 1000, 0.2 * 1000,
+        { mass: 1, frictionAir: 0.2 });
     Matter.World.add(engine.world, playerBody);
 
     const renderer = new THREE.WebGLRenderer();
@@ -134,32 +138,24 @@ const buildMap = (m) => {
         }
     });
 
-    // let vx = 0, vy = 0;
-    const a = 1, v = 40, miu = 4;
+    const a = 0.1;
     let last = 0;
     const animate = (now) => {
         if (!last) last = now;
         const elapsed = now - last;
         last = now;
         
-        let ax = (leftDown ? -a : 0) + (rightDown ? a : 0);
-        let ay = (downDown ? -a : 0) + (upDown ? a : 0);
-        // ax -= vx * miu;
-        // ay -= vy * miu;
-        // Matter.Body.applyForce(playerBody, playerBody.position, { x: ax, y: ay });
+        Matter.Body.applyForce(playerBody, playerBody.position,
+            {
+                x: (leftDown ? -a : 0) + (rightDown ? a : 0),
+                y: (downDown ? a : 0) + (upDown ? -a : 0),
+            });
 
         Matter.Engine.update(engine, elapsed);
-        // vx += elapsed * ax;
-        // vy += elapsed * ay;
-        // vx = Math.min(Math.max(vx, -v), v);
-        // vy = Math.min(Math.max(vy, -v), v);
-        // camera.position.x = playerBody.position.x;
-        // camera.position.y = playerBody.position.y;
-        console.log(playerBody.position);
-        // camera.position.x += vx * elapsed;
-        // camera.position.y += vy * elapsed;
-        // player.position.x += vx * elapsed;
-        // player.position.y += vy * elapsed;
+        camera.position.x = playerBody.position.x / 1000;
+        camera.position.y = -playerBody.position.y / 1000;
+        player.position.x = playerBody.position.x / 1000;
+        player.position.y = -playerBody.position.y / 1000;
 
         renderer.render(scene, camera);
         requestAnimationFrame(animate);
