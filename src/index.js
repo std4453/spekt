b2669c7be2eacff776c8099b5101162eadc83634
Haichuan Zhang <me@std4453.com>
2019-05-21 23:10:45 +0800

    Blend

--- src/index.js
+++ src/index.js
@@ -31,12 +31,12 @@ const buildMap = (m) => {
                 if (begin < 0) begin = x;
             } else {
                 if (begin >= 0) {
-                    edges.push([begin, height - y], [x, height - y]);
+                    edges.push([begin, height - y, x, height - y]);
                     begin = -1;
                 }
             }
         }
-        if (begin >= 0) edges.push([begin, height - y], [width, height - y]);
+        if (begin >= 0) edges.push([begin, height - y, width, height - y]);
     }
     for (let x = 0; x <= width; ++x) {
         let begin = -1;
@@ -47,14 +47,31 @@ const buildMap = (m) => {
                 if (begin < 0) begin = y;
             } else {
                 if (begin >= 0) {
-                    edges.push([x, height - begin], [x, height - y]);
+                    edges.push([x, height - begin, x, height - y]);
                     begin = -1;
                 }
             }
         }
-        if (begin >= 0) edges.push([x, height - begin], [x, height - height]);
+        if (begin >= 0) edges.push([x, height - begin, x, height - height]);
     }
-    return { width, height, sx, sy: height - sy, edges };
+
+    const tiles = [], queue = [[sx, sy]], visited = new Array(height);
+    for (let i = 0; i < height; ++i) visited[i] = new Array(width);
+    while (queue.length > 0) {
+        const [x, y] = queue.splice(0, 1)[0];
+        tiles.push([x, height - 1 - y, x + 1, height - y]);
+        visited[y][x] = true;
+
+        [[0, 1], [0, -1], [1, 0], [-1, 0]].forEach(([dx, dy]) => {
+            const nx = x + dx, ny = y + dy;
+            if (nx < 0 || nx >= width || ny < 0 || ny >= height) return;
+            if (visited[ny][nx]) return;
+            if (lines[ny][nx] !== '-') return;
+            queue.push([nx, ny]);
+        });
+    }
+
+    return { width, height, sx, sy: height - sy, edges, tiles };
 }
 
 (() => {
@@ -62,15 +79,19 @@ const buildMap = (m) => {
     const engine = Matter.Engine.create();
     engine.world.gravity.y = 0;
 
-    const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
-    const { sx, sy, edges } = buildMap(gameMap);
-    for (let i = 0; i < edges.length; i += 2) {
-        const [x1, y1] = edges[i], [x2, y2] = edges[i + 1];
+    const wallMat = new THREE.MeshBasicMaterial({
+        color: 0x777777,
+        blending: THREE.CustomBlending,
+        blendEquation: THREE.MinEquation,
+        transparent: true,
+    });
+    const { sx, sy, edges, tiles } = buildMap(gameMap);
+    for (const [x1, y1, x2, y2] of edges) {
         const dx = Math.abs(x1 - x2) + 0.1;
         const dy = Math.abs(y1 - y2) + 0.1;
         const cx = (x1 + x2) / 2, cy = (y1 + y2) / 2;
         const geometry = new THREE.BoxGeometry(dx, dy, 20);
-        const cube = new THREE.Mesh(geometry, material);
+        const cube = new THREE.Mesh(geometry, wallMat);
         cube.position.x = cx;
         cube.position.y = cy;
         cube.position.z = 10;
@@ -82,16 +103,26 @@ const buildMap = (m) => {
         Matter.World.add(engine.world, box);
     }
 
+    const tileMat = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
+    for (const [x1, y1, x2, y2] of tiles) {
+        const geometry = new THREE.PlaneGeometry(x2 - x1, y2 - y1);
+        const tile = new THREE.Mesh(geometry, tileMat);
+        tile.position.x = (x1 + x2) / 2;
+        tile.position.y = (y1 + y2) / 2;
+        scene.add(tile);
+    }
+
     const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
     camera.position.z = 20;
     camera.position.x = sx;
     camera.position.y = sy;
 
+    const playerMat = new THREE.MeshBasicMaterial({ color: 0x000000 }); 
     const playerGeometry = new THREE.CircleGeometry(0.2, 64);
-    const player = new THREE.Mesh(playerGeometry, material);
+    const player = new THREE.Mesh(playerGeometry, playerMat);
     player.position.x = sx;
     player.position.y = sy;
-    player.position.z = 0;
+    player.position.z = 0.1;
     scene.add(player);
 
     const playerBody = Matter.Bodies.circle(
@@ -100,7 +131,7 @@ const buildMap = (m) => {
     Matter.World.add(engine.world, playerBody);
 
     const renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(new THREE.Color(0xffffff), 1.0);
+    // renderer.setClearColor(new THREE.Color(0xffffff), 1.0);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
@@ -142,7 +173,7 @@ const buildMap = (m) => {
     let last = 0;
     const animate = (now) => {
         if (!last) last = now;
-        const elapsed = now - last;
+        // const elapsed = now - last;
         last = now;
         
         Matter.Body.applyForce(playerBody, playerBody.position,
@@ -151,7 +182,7 @@ const buildMap = (m) => {
                 y: (downDown ? a : 0) + (upDown ? -a : 0),
             });
 
-        Matter.Engine.update(engine, elapsed);
+        Matter.Engine.update(engine, 1000 / 60);
         camera.position.x = playerBody.position.x / 1000;
         camera.position.y = -playerBody.position.y / 1000;
         player.position.x = playerBody.position.x / 1000;
